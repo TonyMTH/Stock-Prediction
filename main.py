92df6ddd19df52a07c02549d691bc04162c4e650
Nwachukwu Anthony <nwachukwuanthony2015@gmail.com>
2021-12-01 20:31:01 +0100

    include main file

---
+++ main.py
@@ -0,0 +1,28 @@
+import datetime
+
+import streamlit as st
+import pandas as pd
+
+from predict import PredictStock
+
+# Text/Title
+st.title("Stock Prediction ")
+
+model_pth = 'data/best_model.pt'
+
+uploaded_file = st.file_uploader('Upload Stock File')
+if uploaded_file is not None:
+    data = pd.read_csv(uploaded_file)
+    st.dataframe(data)
+
+    if st.button('Predict'):
+        out = PredictStock(model_pth).predict(data)
+
+        date = datetime.datetime.strptime(data['date'].values[-1], "%d-%b-%y")
+        date = date + datetime.timedelta(days=1)
+
+        st.write('ISE.1 Market Price for {} is:\t\t{:.4f}'.format(str(date).split(" ")[0], out))
+
+else:
+    st.write(f'Please upload a file')
+
